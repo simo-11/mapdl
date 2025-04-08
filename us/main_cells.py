05e866894115eafa63bcedb143beb8fb97b3b0db
Simo Nikula <simo.nikula@gmail.com>
2025-04-08 22:00:45 +0300

    us continued

--- us/main_cells.py
+++ us/main_cells.py
@@ -40,11 +40,11 @@ print(f"U with h={h}, w={w}, t={t} defined, do_plots={do_plots}")
 mapdl.clear()
 mapdl.prep7()
 mapdl.et(1,"BEAM188")
-mapdl.keyopt(1,1,1)
-mapdl.keyopt(1,3,3)
-mapdl.keyopt(1,4,2)
-mapdl.keyopt(1,7,2)
-mapdl.keyopt(1,9,3)
+mapdl.keyopt(1,1,1)#Warping included
+#mapdl.keyopt(1,3,3)#Cubic shape functions
+#mapdl.keyopt(1,4,2)#Combined shear stresses 
+#mapdl.keyopt(1,7,2)#Output stresses at integration points
+#mapdl.keyopt(1,9,3)#Output extrapolated stresses at nodes
 mapdl.mp("EX",1,E)
 mapdl.mp("PRXY",1,nu)
 secid=1
@@ -66,7 +66,7 @@ print("Beam model created")
 # %% beam horizontal force
 mapdl.prep7()
 mapdl.fkdele('ALL','ALL')
-mapdl.f(2,"FY",force)
+mapdl.fk(2,"FY",force)
 mapdl.run("/solu")
 mapdl.antype("static")
 mapdl.solve()
@@ -75,7 +75,7 @@ print("Horizontal load processed")
 # %% beam vertical force
 mapdl.prep7()
 mapdl.fkdele('ALL','ALL')
-mapdl.f(2,"FZ",force)
+mapdl.fk(2,"FZ",force)
 mapdl.run("/solu")
 mapdl.antype("static")
 mapdl.solve()
@@ -86,7 +86,7 @@ if moment is None:
     raise Exception(no_moment_defined)
 mapdl.prep7()
 mapdl.fkdele('ALL','ALL')
-mapdl.f(2,"MX",moment)
+mapdl.fk(2,"MX",moment)
 mapdl.run("/solu")
 mapdl.antype("static")
 mapdl.solve()
@@ -95,15 +95,26 @@ print("Torsional load processed")
 # %% plot results
 def get_sorted_node_numbers(result):
     nnum=result.mesh.nnum
+    nodes=result.mesh.nodes
+    ss=nnum.tolist()
+    ss.sort(key=lambda c:nodes[c-1][0])
+    return ss
 
 def plot_result(fig,ax,result,index,label):
     sorted_node_numbers=get_sorted_node_numbers(result)
-    nodes=result.mesh.nodes
-    nd=result.nodal_displacement(0)[1]
+    size=result.mesh.nnum.size
+    xvs=result.mesh.nodes[:,0]
+    nd=result.nodal_displacement(0)[1][:,index]
+    xv=np.zeros(size).tolist()
+    yv=np.zeros(size).tolist()
+    i=0
     for nn in sorted_node_numbers:
-        xv[nn]=nodes[nn]
-        yv[nn]=nd[nn][index]
+        ni=nn-1
+        xv[i]=xvs[ni]
+        yv[i]=abs(nd[ni])
+        i=i+1
     ax.plot(xv,yv,label=label)
+    ax.legend()
 fig_hf, ax_hf = plt.subplots(num='horizontal force',clear=True)
 ax_hf.set_xlabel(r'x-coordinate [m]')
 ax_hf.set_ylabel(r'horizontal displacement [m]')
@@ -111,6 +122,8 @@ plot_result(fig_hf,ax_hf,r_bhf,1,'beam')
 fig_vf, ax_vf = plt.subplots(num='vertical force',clear=True)
 ax_vf.set_xlabel(r'x-coordinate [m]')
 ax_vf.set_ylabel(r'vertical displacement [m]')
+plot_result(fig_vf,ax_vf,r_bvf,2,'beam')
 fig_t, ax_t = plt.subplots(num='torsion',clear=True)
 ax_t.set_ylabel(r'x-coordinate [m]')
 ax_t.set_ylabel(r'rotation [radians]')
+plot_result(fig_t,ax_t,r_bt,3,'beam')
