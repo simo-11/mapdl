00cfbbbb6263f92f8550d7f2e17e9f01b5c403cc
Simo Nikula <simo.nikula@gmail.com>
2025-05-27 22:01:02 +0300

    us: added BOX

--- us/main_cells.py
+++ us/main_cells.py
@@ -4,7 +4,9 @@ Created on Mon Apr  7 10:48:02 2025
 
 @author: simo nikula
 
-Create, solve and report results using ansys models for cantilever U-section
+Create, solve and report results using ansys models for cantilever 
+ * U-section
+ * BOX
 """
 # %% settings and commons initializations
 from ansys.mapdl import core as pymapdl
@@ -13,9 +15,16 @@ import numpy as np
 import types
 import copy
 import re
+import enum
+
+class Section(enum.Enum):
+    U=1
+    BOX=2
+    
 def pick_results():
     mapdl.run("/solu")
     mapdl.antype("static")
+    mapdl.nlgeom(key="on")
     mapdl.solve()
     r=mapdl.result
     return types.SimpleNamespace(
@@ -25,48 +34,94 @@ def pick_results():
 def get_sec_property(name):
     m=re.search(f'{name}\\s*=\\s*([-+\\d\\.E]+)',secdata)
     return float(m.group(1))
-
-h=0.1
-w=0.05
-t=0.004
+section=Section.BOX
 E=210E9
+L=2
+ndiv=20
+match section.name:
+    case "U":
+        h=0.1
+        w=0.05
+        t=0.004
+        force=-1180
+        force_y=t/2
+        moment=None
+    case "BOX":
+        E=1E9
+        h=0.08
+        w=0.16
+        t=0.01
+        moment=1000
+        force=None
+    case _:
+        raise Exception(f'parameters for {section.name} are not defined')    
 nu=0.3
 G=E/(2*(1+nu))
 sharp_corners=True
 do_plots=False
 if not do_plots:
     plt.close('all')
-L=2
-ndiv=20
-force=-1180
-force_y=t/2
-try:
-    mapdl
+if 'mapdl' in vars():
     try:
-        mapdl.clear()
+        mapdl.clear()#noqa
     except pymapdl.errors.MapdlExitedError:
         del mapdl        
-except NameError:
+if not 'mapdl' in vars():
     try:
         mapdl=pymapdl.Mapdl(timeout=5)
     except pymapdl.errors.MapdlConnectionError:
         mapdl=pymapdl.launch_mapdl();
-print(f"U with h={h}, w={w}, t={t} defined, do_plots={do_plots}")
+print(f"""{section.name} with h={h}, w={w}, t={t} defined
+  E={E:.3G}, do_plots={do_plots}""")
 # %% Beam model
+# https://ansyshelp.ansys.com/public/account/secured?returnurl=////Views/Secured/corp/v242/en/ans_elem/Hlp_E_BEAM188.html
 mapdl.clear()
 mapdl.prep7()
 mapdl.et(1,"BEAM188")
-mapdl.keyopt(1,1,1)#Warping included
-#mapdl.keyopt(1,3,3)#Cubic shape functions
-#mapdl.keyopt(1,4,2)#Combined shear stresses 
-#mapdl.keyopt(1,7,2)#Output stresses at integration points
-#mapdl.keyopt(1,9,3)#Output extrapolated stresses at nodes
+mapdl.keyopt(1,1,1)
+# 1=Warping included
+#mapdl.keyopt(1,2,0)
+# Cross-section scaling for nlgeom
+#mapdl.keyopt(1,3,3)
+# Cubic shape functions, 0=Linear, 1=Quadratic
+#mapdl.keyopt(1,4,0)
+# 0=torsion related, 1=transverese, 2=Combined shear stresses 
+#mapdl.keyopt(1,5,0)
+# 0=3D, 1=XY
+#mapdl.keyopt(1,6,0)
+# 0=Output section forces/moments and strains/curvatures at 
+#   integration points along the length (default)
+#mapdl.keyopt(1,7,0)
+# 0=None, 1=Max and min, 2=Output stresses at each section point
+#mapdl.keyopt(1,9,0)
+# 0=None
+# 1=Maximum and minimum stresses/strains
+# 2=1 plus stresses and strains along the exterior 
+#   boundary of the cross-section
+# 3=1 plus stresses and strains at all section nodes
+#mapdl.keyopt(1,11,0)
+# 0=Automatically determine if preintegrated section properties can be used
+# 1=Use numerical integration of section
+#mapdl.keyopt(1,12,0)
+# 0=Linear tapered section analysis; cross-section properties 
+#   are evaluated at each Gauss point
+# 1=Average cross-section analysis
+#mapdl.keyopt(1,15,0)
+# 0=Store averaged results at each section corner node
+# 1=Store non-averaged results at each section integration point.
 mapdl.mp("EX",1,E)
 mapdl.mp("PRXY",1,nu)
 secid=1
-mapdl.sectype(secid,"BEAM","CHAN","U",5)
-#mapdl.secoffset("SHRC")#CENT,SHRC,ORIGIN,USER
-secdata=mapdl.secdata(w,w,h,t,t,t)
+match section.name:
+    case "U":
+        mapdl.sectype(secid,"BEAM","CHAN",section.name,5)
+        #mapdl.secoffset("SHRC")#CENT,SHRC,ORIGIN,USER
+        secdata=mapdl.secdata(w,w,h,t,t,t)
+    case "BOX":
+        mapdl.sectype(secid,"BEAM","HREC",section.name,5)
+        secdata=mapdl.secdata(w,h,t,t,t,t)
+    case _:
+        raise Exception(f'parameters for {section.name} are not defined')    
 mapdl.k(1)
 mapdl.k(2,L)
 mapdl.lstr(1,2)
@@ -80,23 +135,26 @@ if do_plots:
             show_bounds=True, point_size=10)
 print("Beam model created")
 # %% beam horizontal force
-mapdl.prep7()
-mapdl.fkdele('ALL','ALL')
-mapdl.fk(2,"FY",force)
-r_bhf=pick_results()
-print("Horizontal load processed")
+if force:
+    mapdl.prep7()
+    mapdl.fkdele('ALL','ALL')
+    mapdl.fk(2,"FY",force)
+    r_bhf=pick_results()
+    print("Horizontal load processed")
 # %% beam vertical force
 # add moment to cancel effect of torsion
-moment=-force*(
-    get_sec_property('Centroid Y')-get_sec_property('Shear Center Y'))
-mapdl.prep7()
-mapdl.fkdele('ALL','ALL')
-mapdl.fk(2,"FZ",force)
-mapdl.fk(2,"MX",moment)
-r_bvf=pick_results()
-print("Vertical load processed")
+if force:
+    moment=-force*(
+        get_sec_property('Centroid Y')-get_sec_property('Shear Center Y'))
+    mapdl.prep7()
+    mapdl.fkdele('ALL','ALL')
+    mapdl.fk(2,"FZ",force)
+    mapdl.fk(2,"MX",moment)
+    r_bvf=pick_results()
+    print("Vertical load processed")
 # %% beam torsion if vertical load applied at force_y
-moment=force*(force_y-get_sec_property('Shear Center Y'))
+if not moment:
+    moment=force*(force_y-get_sec_property('Shear Center Y'))
 mapdl.prep7()
 mapdl.fkdele('ALL','ALL')
 mapdl.fk(2,"MX",moment)
@@ -146,24 +204,27 @@ def add_analytical_torsion(ax,It,Iw):
     xv=np.linspace(0,L)
     yv=theta(moment,It,Iw,L,xv)
     ax.plot(xv,yv,label='analytical')
-    
-fig_hf, ax_hf = plt.subplots(num='horizontal force',clear=True)
-ax_hf.set_xlabel(r'x-coordinate [m]')
-ax_hf.set_ylabel(r'horizontal displacement [m]')
-plot_result(fig_hf,ax_hf,r_bhf,1,'beam188')
-add_analytical_bending(ax_hf,get_sec_property('Izz'))
-ax_hf.legend()
-fig_vf, ax_vf = plt.subplots(num='vertical force',clear=True)
-ax_vf.set_xlabel(r'x-coordinate [m]')
-ax_vf.set_ylabel(r'vertical displacement [m]')
-plot_result(fig_vf,ax_vf,r_bvf,2,'beam188')
-add_analytical_bending(ax_vf,get_sec_property('Iyy'))
-ax_vf.legend()
-fig_t, ax_t = plt.subplots(num='torsion',clear=True)
-ax_t.set_ylabel(r'x-coordinate [m]')
-ax_t.set_ylabel(r'rotation [radians]')
-plot_result(fig_t,ax_t,r_bt,3,'beam188')
-add_analytical_torsion(ax_t,
-                       get_sec_property('Torsion Constant'),
-                       get_sec_property('Warping Constant'))
-ax_t.legend()
+
+if force:    
+    fig_hf, ax_hf = plt.subplots(num='horizontal force',clear=True)
+    ax_hf.set_xlabel(r'x-coordinate [m]')
+    ax_hf.set_ylabel(r'horizontal displacement [m]')
+    plot_result(fig_hf,ax_hf,r_bhf,1,'beam188')
+    add_analytical_bending(ax_hf,get_sec_property('Izz'))
+    ax_hf.legend()
+if force:    
+    fig_vf, ax_vf = plt.subplots(num='vertical force',clear=True)
+    ax_vf.set_xlabel(r'x-coordinate [m]')
+    ax_vf.set_ylabel(r'vertical displacement [m]')
+    plot_result(fig_vf,ax_vf,r_bvf,2,'beam188')
+    add_analytical_bending(ax_vf,get_sec_property('Iyy'))
+    ax_vf.legend()
+if moment:
+    fig_t, ax_t = plt.subplots(num='torsion',clear=True)
+    ax_t.set_ylabel(r'x-coordinate [m]')
+    ax_t.set_ylabel(r'rotation [radians]')
+    plot_result(fig_t,ax_t,r_bt,3,'beam188')
+    add_analytical_torsion(ax_t,
+                           get_sec_property('Torsion Constant'),
+                           get_sec_property('Warping Constant'))
+    ax_t.legend()
