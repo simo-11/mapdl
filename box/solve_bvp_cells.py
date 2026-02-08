c95af6afd827992b28c316be5f483daf599ff793
Simo Nikula <simo.nikula@gmail.com>
2026-02-08 20:55:59 +0200

    bending with supports using solve_bvp continued

--- box/solve_bvp_cells.py
+++ box/solve_bvp_cells.py
@@ -69,47 +69,57 @@ def beam_bending_with_elastic_supports(bc,L=L, fun=None,
                                point_loads=None, 
                                q_func=None,
                                n_points=50):
-    if q_func is None:
-        q_func = lambda x: np.zeros_like(x)
-
-    def q_total(x):
-        q = q_func(x)
-        if point_loads:
-            for xp, P in point_loads:
-                q += P * np.exp(-((x-xp)**2)/(2*1e-4)) / np.sqrt(2*np.pi*1e-4)
-        return q
-
-    if fun is None and gd is None and b is None:
-        def bending_fun(x, y):
-            return np.vstack((y[1], y[2], y[3], q_total(x)/ec))
-        fun=bending_fun
-    elif fun is None:
-        def gbt_fun(x, y):
-            y1, y2, y3, y4 = y
-            dy1 = y2
-            dy2 = y3
-            dy3 = y4
-            dy4 = (q_total(x) - gd * y3 - b * y1) / ec
-            return np.vstack((dy1, dy2, dy3, dy4))
-        fun=gbt_fun
-    
+    can_provide_total_load=False
+    if fun is None:
+        can_provide_total_load=True
+        if q_func is None:
+            q_func = lambda x: np.zeros_like(x)
+        def q_total(x):
+            q = q_func(x)
+            if point_loads:
+                for xp, P in point_loads:
+                    q += P * (np.exp(-((x-xp)**2)/(2*1e-4)) 
+                              / np.sqrt(2*np.pi*1e-4))
+            return q
+        if gd is None and b is None:
+            def bending_fun(x, y):
+                return np.vstack((y[1], y[2], y[3], q_total(x)/ec))
+            fun=bending_fun
+        else:
+            def gbt_fun(x, y):
+                y1, y2, y3, y4 = y
+                dy1 = y2
+                dy2 = y3
+                dy3 = y4
+                dy4 = (q_total(x) - gd * y3 - b * y1) / ec
+                return np.vstack((dy1, dy2, dy3, dy4))
+            fun=gbt_fun
     xs = np.linspace(0, L, n_points)
     ya = np.zeros((4,xs.size))
     sol=solve_bvp(fun, bc, xs, ya)
-    reactions = []
-    total_load = np.trapezoid(q_total(xs), xs)
-    if point_loads:
-        total_load += sum(P for _, P in point_loads)
+    if can_provide_total_load:
+        total_load = np.trapezoid(q_total(xs), xs)
+        if point_loads:
+            total_load += sum(P for _, P in point_loads)
+    else:
+        total_load=None
+        q_total=None
+    return (sol,xs,q_total,total_load)
 
-    return (sol,reactions, xs, q_total,total_load)
-
-def run_example(bc,point_loads,distributed_load,uc,L=L):
-    (res, reactions, xs, q_total, total_load) = (
+def run_example(bc,point_loads=None,
+                gd=None,b=None,
+                distributed_load=None,uc=None,L=L,fun=None,q=None):
+    (res, xs, q_total, total_load) = (
         beam_bending_with_elastic_supports(
-        bc, L=L, ec=E*Ixx,
+        bc, L=L, ec=E*Ixx,gd=gd,b=b,
         point_loads=point_loads,
-        q_func=distributed_load
+        q_func=distributed_load,
+        fun=fun
     ))
+    if q_total:
+        q_plot=q_total
+    else:
+        q_plot=q
     if not res.success:
         raise Exception(f"solution failed, {res.message}")
     w_plot, theta_plot,M_plot, V_plot = [], [], [], []
@@ -129,7 +139,7 @@ def run_example(bc,point_loads,distributed_load,uc,L=L):
     ]
     ,num=f'solve bvp results for {uc}',clear=True)
     ax=axes["q"]
-    ax.plot(xs, q_total(xs), color="green", label="Load")
+    ax.plot(xs, q_plot(xs), color="green", label="Load")
     if point_loads:
         for xp, P in point_loads:
             ax.axvline(x=xp, color="red", linestyle=":", alpha=0.7)
@@ -142,11 +152,6 @@ def run_example(bc,point_loads,distributed_load,uc,L=L):
             ax.axvline(x=xp, color="red", linestyle=":", 
                         alpha=0.7, label="Point load" 
                         if xp==point_loads[0][0] else "")
-    # Annotate reactions
-    for xm, R in reactions:
-        plt.annotate(f"R={R:.1f}", xy=(xm,0), xytext=(xm,0.05),
-                     arrowprops=dict(facecolor='black', shrink=0.05),
-                     ha='center')
     ax=axes["theta"]
     ax.plot(xs, theta_plot, label="Rotation")
     ax.set_ylabel("θ [rad]")
@@ -182,7 +187,8 @@ def bc(ya,yb):
 point_loads=None
 distributed_load = lambda x: -A*rho*g*np.ones_like(x)
 uc='cf_dl'
-res[uc]=run_example(bc,point_loads,distributed_load,uc)
+res[uc]=run_example(bc,
+                    distributed_load=distributed_load,uc=uc,gd=0,b=0)
 # %% Clamped beam with load at free end
 def bc(ya,yb):
     return np.array([
@@ -194,10 +200,10 @@ def bc(ya,yb):
 point_loads = [(1*L, -L*A*rho*g)]
 distributed_load = None
 uc='cf_pl'
-res[uc]=run_example(bc,point_loads,distributed_load,uc)
+res[uc]=run_example(bc,point_loads=point_loads,uc=uc)
 uc='2L_cf_pl'
 point_loads = [(2*L, -2*L*A*rho*g)]
-res[uc]=run_example(bc,point_loads,distributed_load,uc,L=2*L)
+res[uc]=run_example(bc,point_loads=point_loads,uc=uc,L=2*L)
 # %% Own weight with simple supports
 def bc(ya,yb):
     return np.array([
@@ -212,4 +218,25 @@ distributed_load = lambda x: -A*rho*g*np.ones_like(x)
 uc='ss_dl'
 res[uc]=run_example(bc,point_loads,distributed_load,uc)
 uc='2L_ss_dl'
-res[uc]=run_example(bc,point_loads,distributed_load,uc,L=2*L)
+res[uc]=run_example(bc,point_loads,distributed_load,uc=uc,L=2*L)
+# %% Clamped beam with own weight and displacement support at x=L
+def bc(ya,yb):
+    return np.array([
+        ya[0],# displacement(0)=0
+        ya[1],# rotation'(0)=0
+        yb[2],# moment''(L)=0
+        yb[3] # shear'''(L)=0
+    ])
+ks=-A*rho*g*L/1e-6# Total load will cause at most 1e-6 displacement
+def q(x):
+    return -A*rho*g*np.ones_like(x)
+def fun(x, y):
+    k = np.where(x > 2*L, ks, 0) 
+    y1, y2, y3, y4 = y
+    dy1 = y2
+    dy2 = y3
+    dy3 = y4
+    dy4 = (q(x) - k * y1) / E*Ixx
+    return np.vstack((dy1, dy2, dy3, dy4))
+uc='cf_dl_s@L'
+res[uc]=run_example(bc,uc=uc,fun=fun,q=q)
